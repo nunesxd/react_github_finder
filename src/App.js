54ff60ef98cee30d6281c429fe92c1d2511aee9e
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-02-13 08:24:01 -0300

    Correção de bug do diretório anterior

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
+
 function App() {
   return (
-    <div>
-      <h1>Tailwind CSS Text Testing</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={
+          <>
+            <div className='container'>
+              <h1>Tailwind CSS Text Testing</h1>
+            </div>
+          </>
+        } />
+      </Routes>
+    </Router>
   );
 }
 
