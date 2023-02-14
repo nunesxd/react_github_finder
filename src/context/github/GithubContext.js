149054a9ffd8bf6bd42fe1be47185fc9aa0c5f2f
nunesxd <55452280+nunesxd@users.noreply.github.com>
2023-02-14 10:43:31 -0300

    Criação do GithubContext e sua implementação no App

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,35 @@
+import { createContext, useState } from 'react'
+
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({ children }) => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchUsers = async () => {
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+            }
+        });
+
+        const data = await response.json();
+
+        setUsers(data);
+        setLoading(false);
+    }
+
+    return (
+        <GithubContext.Provider
+        value={{
+            users,
+            loading,
+            fetchUsers,
+        }}>
+            {children}
+        </GithubContext.Provider>
+    )
+}
+
+export default GithubContext
